0457884e86678bb9105f761ed402227a79856b8b
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2017-05-04 15:37:08 +0530

    Write .env files based on dirpath :fire:

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
+import fs from 'fs';
+import path from 'path';
 import typpy from 'typpy';
 import iterateObj from 'iterate-object';
 import Err from 'err';
+import mkdirp from 'mkdirp';
 
-export default function envGenerate(obj){
+export function generateEnvArray(obj){
   if(!typpy.is(obj, Object)){
     throw new TypeError("Expected a json object as argument");
   }
   const arr = [];
   iterateObj(obj, (value, name) => {
-     arr.push(`${name}=${JSON.stringify(value)}`)
+     arr.push(`${name}=${value}`)
   });
   return arr;
+}
+
+export default function envGenerate(obj, dir = __dirname){
+  const file = path.join(dir , ".env");
+
+  function write(contents){
+    mkdirp(path.dirname(file), function (err) {
+    if(err){
+      return err;
+    }
+    fs.writeFileSync(file , contents, "utf-8");
+  });
+
+  }
+  const arr = generateEnvArray(obj);
+  write(arr.join("\n"));
 }
--- test/index.js
+++ test/index.js
@@ -1,31 +1,60 @@
 import test from 'ava';
-import envGenerate from '../src';
+import path from 'path';
+import envGenerate, {generateEnvArray} from '../src';
+import rimraf from 'rimraf';
 
-test('envGenerate is defined', t => {
-	t.is(typeof envGenerate, "function");
+const generatedEnvFolder = path.join(__dirname, "../", "__generated__");
+
+test('generateEnvArray is defined', t => {
+	t.is(typeof generateEnvArray, "function");
 });
 
-test('envGenerate throws error when object is not passed as argument', t => {
+test('generateEnvArray throws error when object is not passed as argument', t => {
 
 	const error = t.throws(() => {
-		envGenerate();
+		generateEnvArray();
 	});
 	t.is(error.message, 'Expected a json object as argument');
 
 });
 
-test('envGenerate does not thrown an error when object is passed as argument', t => {
+test('generateEnvArray does not thrown an error when object is passed as argument', t => {
 
 	t.notThrows(() => {
-		envGenerate({});
+		generateEnvArray({});
 	});
 
 });
 
-test('envGenerate returns an array when passed a valid json object', t => {
+test('generateEnvArray returns an array when passed a valid json object', t => {
 
-	const result = envGenerate({ a : 1});
-	t.snapshot(result);
-	t.is(result instanceof Array, true);
-
+	const result = generateEnvArray({ a : 1});
+	t.is(result[0], "a=1");
+;
 });
+
+test('envGenerate writes a file', t => {
+	const writeFolder =  path.join(generatedEnvFolder, "test1")
+	envGenerate({ a : 1}, writeFolder);
+	t.is(true, true);
+})
+
+test('envGenerate test2', t => {
+	const writeFolder =  path.join(generatedEnvFolder, "test2")
+	envGenerate({
+		"NODE_ENV" : "development",
+		"API_KEY" : "Z4HTfmfL*&*EXBwe%HYt8E9OwZs&9lRQ"
+	}, writeFolder);
+	t.is(true, true);
+})
+
+
+test('envGenerate test3', t => {
+	const writeFolder =  path.join(generatedEnvFolder, "test3")
+	envGenerate({
+		"NODE_ENV" : "development",
+		"API_KEY" : "Z4HTfmfL*&*EXBwe%HYt8E9OwZs&9lRQ",
+		"EMPTY" : "",
+	}, writeFolder);
+	t.is(true, true);
+})
